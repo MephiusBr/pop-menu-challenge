3e343e1f77eefb64722776f85c236d31da83bb00
mephiusbr <matheusprofessionalbrazil@gmail.com>
2025-04-28 18:43:34 -0300

    update MenuItemsController

--- app/controllers/api/menu_items_controller.rb
+++ app/controllers/api/menu_items_controller.rb
@@ -1,14 +1,23 @@
 class Api::MenuItemsController < ApplicationController
   def index
+    unless Restaurant.exists?(params[:restaurant_id]) then
+      return render json: { message: "restaurant doesn't exist" }, status: :bad_request
+    end
+
     unless Menu.exists?(params[:menu_id]) then
       return render json: { message: "menu doesn't exist" }, status: :bad_request
     end
 
-    render json: Menu.with_menu_items.find(params[:menu_id])
-      .as_json(only: [:id, :name], include: { menu_items: { only: [:id, :name, :price] } })
+    render json: Menu.with_menu_items
+      .where(restaurant_id: params[:restaurant_id], id: params[:menu_id])
+      .find(params[:menu_id]).as_json(only: [:id, :name], include: { menu_items: { only: [:id, :name, :price] } })
   end
 
   def show
+    unless Restaurant.exists?(params[:restaurant_id]) then
+      return render json: { message: "restaurant doesn't exist" }, status: :bad_request
+    end
+
     unless Menu.exists?(params[:menu_id]) then
       return render json: { message: "menu doesn't exist" }, status: :bad_request
     end
@@ -17,7 +26,7 @@ class Api::MenuItemsController < ApplicationController
       return render json: { message: "item doesn't exist" }, status: :bad_request
     end
 
-    render json: MenuItem.with_menu.where(id: params[:id]).first
-      .as_json(only: [:id, :name, :price])
+    render json: Menu.with_menu_items.where(id: params[:id]).first
+      .as_json(only: [:id, :name], include: { menu_items: { only: [:id, :name, :price] } })
   end
 end
